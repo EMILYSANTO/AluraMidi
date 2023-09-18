c5ac48ada1ce4379972ed751cc74fad8d8b9ac5b
EMILYSANTO <105228771+EMILYSANTO@users.noreply.github.com>
2023-09-18 08:55:58 -0300

    thanks

---
+++ main.js
@@ -0,0 +1,15 @@
+function tocaSom (idElementoAudio){
+    document.querySelector(idElemetoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador<listaDeTeclas.length;contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = '#som_${instrumento}'; //template string
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+}
